c7572f052a537751aba122ccf673f5f6bc95fd5f
Cassandra Le <cassandrale@Cassandras-MBP.fios-router.home>
2018-01-29 12:42:49 -0500

    DONE: set some functions for dialogflow

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,3 @@
-/**
-* Copyright 2017 Google Inc. All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
 'use strict';
 process.env.DEBUG = 'actions-on-google:*';
 
@@ -21,33 +6,20 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-// const know = admin.database().ref('/animal-knowledge');
-// const graph = know.child('graph');
-
 const status = admin.database().ref('/status');
 
 // Dialogflow Intent names
 const PLAY_INTENT = 'play';
-const NO_INTENT = 'discriminate-no';
-const YES_INTENT = 'discriminate-yes';
-const GIVEUP_INTENT = 'give-up';
-const LEARN_THING_INTENT = 'learn-thing';
-const LEARN_DISCRIM_INTENT = 'learn-discrimination';
+const DETAILS_INTENT = 'details';
+const RECIPE_INTENT = 'findrecipe';
 
 // Contexts
 const WELCOME_CONTEXT = 'welcome';
 const QUESTION_CONTEXT = 'question';
-const GUESS_CONTEXT = 'guess';
-const LEARN_THING_CONTEXT = 'learn-thing';
-const LEARN_DISCRIM_CONTEXT = 'learn-discrimination';
-const ANSWER_CONTEXT = 'answer';
 
 // Context Parameters
 const ID_PARAM = 'id';
 const BRANCH_PARAM = 'branch';
-const LEARN_THING_PARAM = 'learn-thing';
-const GUESSABLE_THING_PARAM = 'guessable-thing';
-const LEARN_DISCRIMINATION_PARAM = 'learn-discrimination';
 const ANSWER_PARAM = 'answer';
 const QUESTION_PARAM = 'question';
 
@@ -59,42 +31,31 @@ exports.assistantcodelab = functions.https.onRequest((request, response) => {
 
    let actionMap = new Map();
    actionMap.set(PLAY_INTENT, play);
-   actionMap.set(NO_INTENT, discriminate);
-   actionMap.set(YES_INTENT, discriminate);
-   actionMap.set(GIVEUP_INTENT, giveUp);
-   actionMap.set(LEARN_THING_INTENT, learnThing);
-   actionMap.set(LEARN_DISCRIM_INTENT, learnDiscrimination);
+   actionMap.set(DETAILS_INTENT, details);
+   actionMap.set(RECIPE_INTENT, findrecipe);
    assistant.handleRequest(actionMap);
 
+   //--------- DESCRIBE TO THE USER THE STATUS OF THE FOOD -------
    function play(assistant) {
        status.once('value', snap => {
-           const speech = `Okay, you currently have ${snap.val().expired} expired food at the moment, ${snap.val().good} food in good status at the moment, and ${snap.val().expired} food that is about to expired soon. Would you like to make a recipe with this food?`;
+           const speech = `Okay, you currently have ${snap.val().expired} expired food, ${snap.val().good} food in good status, and ${snap.val().expired} that is about to expired soon. Would you like to know more details?`;
            assistant.ask(speech);
        });
 
    }
 
-   function discriminate(assistant) {
-       const priorQuestion = assistant.getContextArgument(QUESTION_CONTEXT, ID_PARAM).value;
-       const intent = assistant.getIntent();
-       let yes_no;
-       if (YES_INTENT === intent) {
-           yes_no = 'y';
-       } else {
-           yes_no = 'n';
-       }
-
+   //--------- GET MORE DETAILS ABOUT THE FOOD --------
+   function details(assistant){
+       const speech = `Okay, you have chicken, brocolli, and apple that has already expired. You have chocolate cake about to expire soon, and your donuts are still in good condition. Would you like to get a recipe ?`;
+       assistant.ask(speech);
    }
 
-   function giveUp(assistant) {
 
+   //-------- GET A RECIPE -------
+   function findrecipe(assistant){
+       const speech = `Okay, I found a recipe for you. It's a dish called Lobster Roll. For more information, check out the website for more information. Can I help you with anything else?`;
+       assistant.ask(speech);
    }
 
-   function learnThing(assistant) {
 
-   }
-
-   function learnDiscrimination(assistant) {
-
-   }
 });
