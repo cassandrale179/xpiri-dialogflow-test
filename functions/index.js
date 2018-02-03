c9d44dcd3c8bdcb086bd1f7e8f274eecdc9021e6
Cassandra Le <cassandrale@Cassandras-MBP.fios-router.home>
2018-02-03 13:37:27 -0500

    DONE: install twilio packages

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,46 @@
 'use strict';
 process.env.DEBUG = 'actions-on-google:*';
 
+// --------- LIBRARY TO BE INCLUDED ---------
 const Assistant = require('actions-on-google').ApiAiAssistant;
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const twilio = require('twilio');
 admin.initializeApp(functions.config().firebase);
 
-const storage = admin.database().ref('/storage');
-const count = admin.database().ref('/count');
-const recipetitle = admin.database().ref('/recipetitle');
+//----------- FIREBASE REF ---------------
+const statusRef= admin.database().ref('/status');
+const countRef = admin.database().ref('/count');
+const recipeRef= admin.database().ref('/recipetitle');
 
-// Dialogflow Intent names
+// --------- DIALOGFLOW INTENTS ---------
 const PLAY_INTENT = 'play';
 const DETAILS_INTENT = 'details';
 const RECIPE_INTENT = 'findrecipe';
 
-// Contexts
+// -------- CONTEXT ------------
 const WELCOME_CONTEXT = 'welcome';
 const QUESTION_CONTEXT = 'question';
 
-// Context Parameters
+// -------- CONTEXT PARAMETERS -------
 const ID_PARAM = 'id';
 const BRANCH_PARAM = 'branch';
 const ANSWER_PARAM = 'answer';
 const QUESTION_PARAM = 'question';
 
+//-------- TWILIO API STUFF ----------
+const accountSid = 'ACa25b9e68025c8db22bb5d85e4e930cc5';
+const authToken = 'b3914f78318a81ab0603b02b2256ff93';
+const client = require('twilio')(accountSid, authToken);
+
+//------------------ EXPORT THE FUNCTION -----------------------------
 exports.assistantcodelab = functions.https.onRequest((request, response) => {
    console.log('headers: ' + JSON.stringify(request.headers));
    console.log('body: ' + JSON.stringify(request.body));
 
    const assistant = new Assistant({request: request, response: response});
 
+   //---------- MAPPING THE FUNCTION TO THE ACTIONS ---------
    let actionMap = new Map();
    actionMap.set(PLAY_INTENT, play);
    actionMap.set(DETAILS_INTENT, details);
@@ -39,8 +49,8 @@ exports.assistantcodelab = functions.https.onRequest((request, response) => {
 
    //--------- DESCRIBE TO THE USER THE STATUS OF THE FOOD -------
    function play(assistant) {
-       count.once('value', snap => {
-           const speech = `Okay, you currently have ${snap.val().expired} expired food, ${snap.val().good} food in good status, and ${snap.val().expired} that is stale. Would you like to know more details?`;
+       countRef.once('value', snap => {
+           const speech = `Okay, you currently have ${snap.val().e} expired food, ${snap.val().g} food in good status, and ${snap.val().s} that is stale. Would you like to know more details?`;
            assistant.ask(speech);
        });
 
@@ -48,17 +58,25 @@ exports.assistantcodelab = functions.https.onRequest((request, response) => {
 
    //--------- GET MORE DETAILS ABOUT THE FOOD --------
    function details(assistant){
-       const speech = `Okay, you have chicken, brocolli, and apple that has already expired. You have chocolate cake about to expire soon, and your donuts are still in good condition. Would you like to get a recipe ?`;
-       assistant.ask(speech);
+       statusRef.once('value', snap => {
+           const speech = `Okay, you currently have ${snap.val().good} that are good, ${snap.val().stale} that are stale, and
+           ${snap.val().expired} that are expired. What else you would like to do?`;
+           assistant.ask(speech);
+       });
    }
 
 
    //-------- GET A RECIPE -------
    function findrecipe(assistant){
-       recipetitle.once('value', snap => {
-           const speech = `Okay, I found a recipe for you. It's a dish called ${snap.val().title} For more information, check out the website for more information. Can I help you with anything else?`;
+       recipeRef.once('value', snap => {
+           const speech = `Okay, I found a recipe for you. It's a dish called ${snap.val().title} For more information, check out the website. Can I help you with anything else?`;
            assistant.ask(speech);
        });
+   }
+
+//  ------ SEND A TEXT MESSAGE -----
+   function sendmessage(assistant){
+
    }
 
 });
